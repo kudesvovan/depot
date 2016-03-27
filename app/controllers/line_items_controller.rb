32957f6901936b40b08bc2d1c0437b397ac4ee5d
vladimir vasilyev <vladi_it@mail.ru>
2016-03-27 20:10:44 +0300

    10.3 chapter

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -3,7 +3,6 @@ class LineItemsController < ApplicationController
   include PageVisitCounter
 
   before_action :set_cart, only: [:create]
-
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -30,11 +29,11 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product.id)
     respond_to do |format|
       if @line_item.save
         reset_counter
-        format.html { redirect_to @line_item.cart , notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
@@ -75,6 +74,6 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
