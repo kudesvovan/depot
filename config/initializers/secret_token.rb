4a9c4c19b0c29cc8d52616f9bf68ca75b68edc23
vladimir vasilyev <vladi_it@mail.ru>
2016-04-17 13:34:50 +0300

    gitignore secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Depot::Application.config.secret_key_base = '864f01222a08c9c985fd88ff183a8da522cbf2acc01da1dc394f84b2f53e6a98aadbbb9222517c035044f2df31fdf4994c73b57100f10c229830e6ad8d593f52'
+Depot::Application.config.secret_key_base = ENV["SECRET_TOKEN"]
